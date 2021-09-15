5a7802e39768c3c6375d6ed394029f6506a5d312
jae <wogud1514@naver.com>
2021-09-16 00:05:52 +0900

    시큐리티 config까지 설정

---
+++ src/main/java/com/example/security/controller/IndexController.java
@@ -0,0 +1,52 @@
+package com.example.security.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @ResponseBody
+    @GetMapping({"","/"})
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @ResponseBody
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @ResponseBody
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @ResponseBody
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @ResponseBody
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "회원가입완료됨";
+    }
+}
